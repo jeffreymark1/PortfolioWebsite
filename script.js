8ab0ecd0ee5a6dd887b2bc03757dab260ccedffb
Jeff G <jeffg@Jeffs-MBP.hsd1.pa.comcast.net>
2023-12-04 01:00:21 -0500

    Changed font styling, changed btn styling, added EventListener &  click event function, changed cursor styling

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("JavaScript is linked to HTML!");
+
+const ppb = document.querySelector(".portfolio_projects_box");
+
+ppb.addEventListener ('click', () => {
+    window.open("https://odysseynoir.netlify.app/", "_blank");
+})
