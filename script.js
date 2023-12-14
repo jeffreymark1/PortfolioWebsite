460d2c8051706833825196b65dbd2d4febf57316
Jeff G <jeffg@Jeffs-MacBook-Pro.local>
2023-12-14 15:03:01 -0500

    Added @media(max-width:390px) for mobile phone with smaller screens

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 console.log("JavaScript is linked to HTML!");
 
-const ppb = document.querySelector(".portfolio_projects_box");
+const ppb = document.querySelectorAll(".black");
+console.log(ppb);
 
-ppb.addEventListener ('click', () => {
+for(let i = 0; i <= ppb.length - 1; i++){
+    ppb.addEventListener ("click", () => {
+        window.open("https://odysseynoir.netlify.app/", "_blank");
+    })
+}
+
+/*ppb.addEventListener ('click', (e) => {
     window.open("https://odysseynoir.netlify.app/", "_blank");
-})
+})*/
