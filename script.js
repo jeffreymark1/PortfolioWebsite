b18275f3efcf57038795ad43a8504aae0ff4130b
Jeff G <jeffg@Jeffs-MBP.hsd1.pa.comcast.net>
2023-12-18 03:18:17 -0500

    Resize elements for @media(max-width:390px)

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 console.log("JavaScript is linked to HTML!");
 
-const ppb = document.querySelectorAll(".black");
+const ppb = document.querySelector(".black");
 console.log(ppb);
 
-for(let i = 0; i <= ppb.length - 1; i++){
-    ppb.addEventListener ("click", () => {
-        window.open("https://odysseynoir.netlify.app/", "_blank");
-    })
-}
-
-/*ppb.addEventListener ('click', (e) => {
+ppb.addEventListener ('click', (e) => {
     window.open("https://odysseynoir.netlify.app/", "_blank");
-})*/
+})
