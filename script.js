a0c12753765903053c0e369efb7f43c1360054d8
Jeff G <jeffg@Jeffs-MBP.hsd1.pa.comcast.net>
2023-12-24 02:31:53 -0500

    Added Query Selectors and EventListeners

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-console.log("JavaScript is linked to HTML!");
+//Query Selectors
+const ppbOne = document.querySelector(".boxOne");
+const ppbTwo = document.querySelector(".boxTwo");
+const ppbThree = document.querySelector(".boxThree");
+const ppbFour = document.querySelector(".boxFour");
+const ppbFive = document.querySelector(".boxFive");
 
-const ppb = document.querySelector(".black");
-console.log(ppb);
+console.log(ppbFour);
 
-ppb.addEventListener ('click', (e) => {
+//EventListeners
+ppbOne.addEventListener ('click', (e) => {
     window.open("https://odysseynoir.netlify.app/", "_blank");
+})
+
+ppbTwo.addEventListener ('click', (e) => {
+    window.open("https://fashionblogproject.netlify.app", "_blank");
+})
+
+ppbThree.addEventListener ('click', (e) => {
+    window.open("https://openweathersite.netlify.app", "_blank");
+})
+
+ppbFour.addEventListener ('click', (e) => {
+    window.open("https://virgingalacticparallax.netlify.app", "_blank");
+})
+
+ppbFive.addEventListener ('click', (e) => {
+    window.open("https://github.com/jeffreymark1/", "_self");
 })
